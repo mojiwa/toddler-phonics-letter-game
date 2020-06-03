3bb6c67e93ee3d648f100d042b603e7bdb6af00f
Mo Jiwa <mohammed.jiwa@gmail.com>
2020-06-03 15:59:30 +0100

    remove google authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import { GoogleLogout } from 'react-google-login';
-
-import Auth from './secret';
 
 class App extends React.Component {
-
-  onLoggedOut() {    
-    localStorage.setItem('loggedIn', 'false');
-  }
 
   render() {
     return (
       <div>
-        <GoogleLogout
-          clientId={Auth.Google.CLIENT_ID}
-          buttonText="Logout"
-          onLogoutSuccess={this.onLoggedOut}
-          onFailure={() => console.log('Failed')} />      
+        
       </div>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -2,69 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import GoogleLogin from 'react-google-login';
 
-import Auth from './secret';
 import './styles/main.css';
 import './styles/custom.css';
 
-interface ILoadAppState {
-  IsLoggedIn: Boolean;
-}
-
-class LoadApp extends React.Component<{}, ILoadAppState> {
-  readonly state = {
-    IsLoggedIn: false
-  }
-
-  componentDidMount = () => {
-    if (this.isClientLoggedIn())
-      this.setState({IsLoggedIn: true});
-  }
-
-  storageSupported(): boolean {
-    return typeof(Storage) !== 'undefined';
-  }
-  
-  isClientLoggedIn(): boolean {
-    return localStorage.getItem('loggedIn') === 'true';
-  }
-  
-  onSuccess = () => {              
-    localStorage.setItem('loggedIn', 'true');
-    this.setState({IsLoggedIn: true});
-  }
-  
-  onFailure(response: any) {        
-    console.log(response);
-    localStorage.setItem('loggedIn', 'false');
-    this.setState({IsLoggedIn: false});
-  }
-
-  render() {
-    if (!this.storageSupported()) {
-      return (
-        <div>Please use a web browser that supports HTML5</div>
-      );
-    }
-    else {      
-      return (
-        this.state.IsLoggedIn ? 
-          <App />  :            
-          <GoogleLogin
-            clientId={Auth.Google.CLIENT_ID}
-            buttonText="Login with your Google account..."
-            onSuccess={() => this.onSuccess()}
-            onFailure={(response: any) => this.onFailure(response)}
-            cookiePolicy={'single_host_origin'} />
-      )      
-    }
-  }
-}
-
 ReactDOM.render(
   <React.StrictMode>
-    <LoadApp />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
